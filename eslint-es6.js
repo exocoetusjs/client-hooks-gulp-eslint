25f8e3b8dc0c1ba242dea15c831b7558e883543b
crux.wild <18210841568@163.com>
2016-08-07 02:48:35 +0800

    add

--- eslint-es6.js
+++ eslint-es6.js
@@ -21,11 +21,15 @@ try {
     process.stderr.write('please install [gulp] first.\n');
   }
 
+  if (shell.exec(`export FORCE_COLOR='true'`).code !== 0) {
+    process.stderr.write('export FORCE_COLOR => execution failed. \n');
+  }
+
   if (shell.exec(`export CWD=${cwd}`).code !== 0) {
     process.stderr.write('export CWD => execution failed. \n');
   }
 
-  if (shell.exec(`gulp --color --gulpfile ${gulpPath} 2>&1`).code !== 0) {
+  if (shell.exec(`gulp --gulpfile ${gulpPath} 2>&1`).code !== 0) {
     process.stderr.write('gulp eslint-es6 => execution failed. \n');
   }
 }
