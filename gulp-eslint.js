6f57409b1ef19999bf339e3e11067cc49660dee5
jieling.guan <jieling.guan@jimu.com>
2016-08-18 17:01:10 +0800

    add

--- gulp-eslint.js
+++ gulp-eslint.js
@@ -100,105 +100,106 @@ module.exports = {
          */
         allowEmpty: false,
       },
-      /**
-       * @see https://github.com/adametry/gulp-eslint
-       */
-      eslint: {
-        constructor: {
-          /**
-           * @type {Object}
-           *
-           * Set [configuration] of [rules].
-           *
-           * [rules]: http://eslint.org/docs/rules/
-           * [configuration]: http://eslint.org/docs/user-guide/configuring#configuring-rules
-           */
-          rules: {},
-          /**
-           * @type {Array}
-           *
-           * Specify [globals].
-           *
-           * ```javascript
-           * {
-           *   'globals': ['jQuery', '$']
-           * }
-           * ```
-           *
-           * [globals]: http://eslint.org/docs/user-guide/configuring#specifying-globals
-           */
-          globals: {},
-          /**
-           * @type {Boolean}
-           *
-           * This option instructs ESLint to try to fix as many issues as
-           * possible. The fixes are applied to the gulp stream. The fixed
-           * content can be saved to file using `gulp.dest` (See [example/fix.js]).
-           * Rules that are fixable can be found in ESLint's [rules list].
-           *
-           * When fixes are applied, a "fixed" property is set to `true` on the
-           * fixed file's ESLint result.
-           *
-           * [rules list]: http://eslint.org/docs/rules/
-           * [example/fix.js]: https://github.com/adametry/gulp-eslint/blob/master/example/fix.js.
-           */
-          fix: false,
-          /**
-           * @type {Boolean}
-           *
-           * When `true`, this option will filter warning messages from ESLint
-           * results. This mimics the ESLint CLI [quiet option].
-           *
-           * [quiet option](http://eslint.org/docs/user-guide/command-line-interface#quiet).
-           */
-          quiet: false,
-          /**
-           * @type {Array}
-           *
-           * Specify a list of [environments] to be applied.
-           *
-           * [environments]: http://eslint.org/docs/user-guide/configuring#specifying-environments
-           */
-          envs: [],
-          /**
-           * @type {Array}
-           * This option allows you to specify additional directories from which
-           * to load rules files. This is useful when you have custom rules that
-           * aren't suitable for being bundled with ESLint. This option works
-           * much like the ESLint CLI's [rulesdir option].
-           *
-           * [rulesdir option]: http://eslint.org/docs/user-guide/command-line-interface#rulesdir
-           */
-          rulePaths: [],
-          /**
-           * @type {String}
-           *
-           * Path to the ESLint rules configuration file. For more information,
-           * see the ESLint CLI [config option] and [Using Configuration Files].
-           *
-           * [config option]: http://eslint.org/docs/user-guide/command-line-interface#c-config
-           * [Using Configuration Files]: http://eslint.org/docs/user-guide/configuring#using-configuration-files
-           */
-          configFile: '',
-          /**
-           * @type {Boolean}
-           *
-           * When `true`, add a result warning when ESLint ignores a file. This
-           * can be used to file files that are needlessly being loaded by
-           * `gulp.src`. For example, since ESLint automatically ignores
-           * "node_modules" file paths and gulp.src does not, a gulp task may
-           * take seconds longer just reading files from the "node_modules"
-           * directory.
-           */
-          warnFileIgnored: false,
-          /**
-           * @type {Boolean}
-           * When `false`, ESLint will not load [.eslintrc files].
-           * [.eslintrc files]: http://eslint.org/docs/user-guide/configuring#using-configuration-files
-           */
-          useEslintrc: true,
-        },
-      }
     }
-  }
+  },
+  /**
+   * @see https://github.com/adametry/gulp-eslint
+   */
+  eslint: {
+    constructor: {
+      /**
+       * @type {Object}
+       *
+       * Set [configuration] of [rules].
+       *
+       * [rules]: http://eslint.org/docs/rules/
+       * [configuration]: http://eslint.org/docs/user-guide/configuring#configuring-rules
+       */
+      rules: {},
+      /**
+       * @type {Array}
+       *
+       * Specify [globals].
+       *
+       * ```javascript
+       * {
+       *   'globals': ['jQuery', '$']
+       * }
+       * ```
+       *
+       * [globals]: http://eslint.org/docs/user-guide/configuring#specifying-globals
+       */
+      globals: {},
+      /**
+       * @type {Boolean}
+       *
+       * This option instructs ESLint to try to fix as many issues as
+       * possible. The fixes are applied to the gulp stream. The fixed
+       * content can be saved to file using `gulp.dest` (See [example/fix.js]).
+       * Rules that are fixable can be found in ESLint's [rules list].
+       *
+       * When fixes are applied, a "fixed" property is set to `true` on the
+       * fixed file's ESLint result.
+       *
+       * [rules list]: http://eslint.org/docs/rules/
+       * [example/fix.js]: https://github.com/adametry/gulp-eslint/blob/master/example/fix.js.
+       */
+      fix: false,
+      /**
+       * @type {Boolean}
+       *
+       * When `true`, this option will filter warning messages from ESLint
+       * results. This mimics the ESLint CLI [quiet option].
+       *
+       * [quiet option](http://eslint.org/docs/user-guide/command-line-interface#quiet).
+       */
+      quiet: false,
+      /**
+       * @type {Array}
+       *
+       * Specify a list of [environments] to be applied.
+       *
+       * [environments]: http://eslint.org/docs/user-guide/configuring#specifying-environments
+       */
+      envs: [],
+      /**
+       * @type {Array}
+       * This option allows you to specify additional directories from which
+       * to load rules files. This is useful when you have custom rules that
+       * aren't suitable for being bundled with ESLint. This option works
+       * much like the ESLint CLI's [rulesdir option].
+       *
+       * [rulesdir option]: http://eslint.org/docs/user-guide/command-line-interface#rulesdir
+       */
+      rulePaths: [],
+      /**
+       * @type {String}
+       *
+       * Path to the ESLint rules configuration file. For more information,
+       * see the ESLint CLI [config option] and [Using Configuration Files].
+       *
+       * [config option]: http://eslint.org/docs/user-guide/command-line-interface#c-config
+       * [Using Configuration Files]: http://eslint.org/docs/user-guide/configuring#using-configuration-files
+       */
+      configFile: '',
+      /**
+       * @type {Boolean}
+       *
+       * When `true`, add a result warning when ESLint ignores a file. This
+       * can be used to file files that are needlessly being loaded by
+       * `gulp.src`. For example, since ESLint automatically ignores
+       * "node_modules" file paths and gulp.src does not, a gulp task may
+       * take seconds longer just reading files from the "node_modules"
+       * directory.
+       */
+      warnFileIgnored: false,
+      /**
+       * @type {Boolean}
+       * When `false`, ESLint will not load [.eslintrc files].
+       * [.eslintrc files]: http://eslint.org/docs/user-guide/configuring#using-configuration-files
+       */
+      useEslintrc: true,
+    },
+  },
 };
+
--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,20 @@
+const eslint = require('gulp-eslint');
+
 const path = require('path');
 
 const gulp = require('gulp');
 
-const eslint = require('gulp-eslint');
-
 gulp.task('eslint', () => {
+  // get configuration parameters.
+  const config = JSON.parse(process.env.config);
   // ESLint ignores files with "node_modules" paths.
   // So, it's best to have gulp ignore the directory as well.
   // Also, Be sure to return the stream from the task;
   // Otherwise, the task may end before the stream has finished.
-  return gulp.src(['**/*.js','!node_modules/**'], { cwd: process.env.CWD })
+  return gulp.src(config.gulp.src.globs, config.gulp.src.options)
     // eslint() attaches the lint output to the "eslint" property
     // of the file object so it can be used by other modules.
-    .pipe(eslint({ configFile: path.join(__dirname, '.eslintrc') }))
+    .pipe(eslint(config.eslint.constructor))
     // eslint.format() outputs the lint results to the console.
     // Alternatively use eslint.formatEach() (see Docs).
     .pipe(eslint.format())
